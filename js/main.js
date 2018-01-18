333499d20d32c8c961b77c4cca98416fd854322d
Kelsey Pintens <ksepintens@gmail.com>
2018-01-18 10:02:54 -0600

    added basic js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    document.getElementById("work").style.fontWeight = "lighter";
+    document.getElementById("about-me").style.fontWeight = "lighter";
+    document.getElementById("blog").style.fontWeight = "lighter";
+}
